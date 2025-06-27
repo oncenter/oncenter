b5c406c613dd46b9f77acc56b780aac6d4f16289
tatito22 <tatolucero07@gmail.com>
2025-06-26 22:29:18 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Script para toggle menú hamburguesa en móvil
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks
